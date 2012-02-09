dd412cef916fef5618b538ee6e2ac7f5507c8853
Alex Tang <lalextang@gmail.com>
2012-02-09 12:45:39 +0800

    remove unused code

--- src/main/java/com/kwchina/wfm/interfaces/organization/web/UnitController.java
+++ src/main/java/com/kwchina/wfm/interfaces/organization/web/UnitController.java
@@ -1,11 +1,8 @@
 package com.kwchina.wfm.interfaces.organization.web;
 
-import java.util.Locale;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
