cffddbe4d12ba11142f5908eb8e80bdee9472615
Alex Tang <lalextang@gmail.com>
2012-02-24 01:25:54 +0800

    add shift type repository

---
+++ src/main/java/com/kwchina/wfm/domain/model/shift/ShiftTypeRepository.java
@@ -0,0 +1,8 @@
+package com.kwchina.wfm.domain.model.shift;
+
+import com.kwchina.wfm.domain.common.BaseRepository;
+
+public interface ShiftTypeRepository extends BaseRepository<ShiftType> {
+
+	
+}
---
+++ src/main/java/com/kwchina/wfm/infrastructure/persistence/ShiftTypeRepositoryImpl.java
@@ -0,0 +1,15 @@
+package com.kwchina.wfm.infrastructure.persistence;
+
+import org.springframework.stereotype.Repository;
+
+import com.kwchina.wfm.domain.model.shift.ShiftType;
+import com.kwchina.wfm.domain.model.shift.ShiftTypeRepository;
+
+@Repository
+public class ShiftTypeRepositoryImpl extends BaseRepositoryImpl<ShiftType> implements ShiftTypeRepository {
+
+//	@PersistenceContext
+//	private EntityManager entityManager;
+	
+
+}
