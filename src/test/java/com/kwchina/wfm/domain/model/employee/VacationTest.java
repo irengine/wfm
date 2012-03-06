bebd199f6a5d25763778c7de872f58951fce1688
Alex Tang <lalextang@gmail.com>
2012-03-07 02:24:24 +0800

    fix calculate vacation issue

--- src/test/java/com/kwchina/wfm/domain/model/employee/VacationTest.java
+++ src/test/java/com/kwchina/wfm/domain/model/employee/VacationTest.java
@@ -3,6 +3,7 @@ package com.kwchina.wfm.domain.model.employee;
 import static org.junit.Assert.assertTrue;
 
 import org.joda.time.DateTime;
+import org.joda.time.DateTimeFieldType;
 import org.joda.time.Period;
 import org.junit.Test;
 
@@ -10,14 +11,41 @@ import com.kwchina.wfm.infrastructure.common.DateHelper;
 
 public class VacationTest {
 
-
-@Test
+	@Test
 	public void testCalculateMonthDifference() {
 		DateTime start = DateHelper.getDateTime("2009-01-14");
 		DateTime end = DateHelper.getDateTime("2011-01-12");
 		Period p = new Period(start, end);
 		assertTrue( 1 == p.getYears());
 		assertTrue( 11 == p.getMonths());
+		assertTrue( 1 == end.get(DateTimeFieldType.monthOfYear()));
+		
+		int i = 0;
+		i = 13 /5;
+		assertTrue(2 == i);
+		
+		assertTrue( 5 == getDays(p.getYears(), end.get(DateTimeFieldType.monthOfYear())));
+	}
+	
+	private int getDays(int years, int month) {
+		int days = 0;
+		
+		if (years == 0)
+			days = 0;
+		else if (years == 1)
+			days = (5 * (12 - month + 1)) / 12;
+		else if (years > 1 && years < 10)
+			days = 5;
+		else if (years == 10)
+			days = 5 * (month - 1) / 12 + 10 * (12 - month + 1) /12 ;
+		else if (years > 10 && years < 20)
+			days = 10;
+		else if (years == 20)
+			days = 10 * (month - 1) / 12 + 15 * (12 - month + 1) /12 ;
+		else if (years > 20)
+			days = 15;
+		
+		return days;
 	}
 
 }
