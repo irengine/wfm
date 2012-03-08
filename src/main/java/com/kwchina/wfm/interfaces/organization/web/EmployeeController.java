b6654bc493a3611a3d264df109c4353ac9b77ba3
Alex Tang <lalextang@gmail.com>
2012-03-08 16:39:45 +0800

    empty calculate vacation

--- src/main/java/com/kwchina/wfm/interfaces/organization/web/EmployeeController.java
+++ src/main/java/com/kwchina/wfm/interfaces/organization/web/EmployeeController.java
@@ -99,4 +99,9 @@ public class EmployeeController {
 		employeeServiceFacade.saveTimeSheetRecord(command);
 	}
 
+	@RequestMapping(value = "/calculateVacation", method = RequestMethod.GET)
+	public void calculateVacation(HttpServletRequest request, HttpServletResponse response) {
+
+	}
+
 }
