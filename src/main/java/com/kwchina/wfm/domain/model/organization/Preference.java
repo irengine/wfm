5b42ba1423622893154cff0dda4ccf39a5b1b6da
Alex Tang <lalextang@gmail.com>
2012-03-10 14:00:06 +0800

    extends value object

--- src/main/java/com/kwchina/wfm/domain/model/organization/Preference.java
+++ src/main/java/com/kwchina/wfm/domain/model/organization/Preference.java
@@ -2,9 +2,16 @@ package com.kwchina.wfm.domain.model.organization;
 
 import javax.persistence.Embeddable;
 
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
+import com.kwchina.wfm.domain.common.ValueObject;
+
 @Embeddable
-public class Preference {
+public class Preference implements ValueObject<Preference> {
 	
+	private static final long serialVersionUID = 7093966149398348885L;
+
 	private String key;
 	
 	private String value;
@@ -44,4 +51,31 @@ public class Preference {
 	public void setScope(String scope) {
 		this.scope = scope;
 	}
+	
+	@Override
+	public boolean sameValueAs(final Preference other) {
+		return other != null
+				&& new EqualsBuilder().append(this.scope, other.scope)
+						.append(this.key, other.key)
+						.append(this.value, other.value).isEquals();
+	}
+
+	@Override
+	public boolean equals(final Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+
+		final Preference other = (Preference) o;
+
+		return sameValueAs(other);
+	}
+
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder().append(scope).append(key).append(value)
+				.toHashCode();
+	}
+	  
 }
