6196a6eb7593978b2a7fc37bf0ab2ad40daa2e22
Alex Tang <lalextang@msn.com>
2012-03-16 11:01:16 +0800

    fix only show enable unit

--- src/main/java/com/kwchina/wfm/domain/model/organization/Unit.java
+++ src/main/java/com/kwchina/wfm/domain/model/organization/Unit.java
@@ -25,8 +25,8 @@ import com.kwchina.wfm.domain.model.shift.ShiftType;
 @Table(name="T_UNITS")
 @NamedQueries({
 	@NamedQuery(name = "unit.findRoot", query = "SELECT u FROM Unit u WHERE u.parent = null"),
-	@NamedQuery(name = "unit.findAllChildren", query = "SELECT u FROM Unit u WHERE u.left > :parentLeft and u.left < :parentRight order by u.left"),
-	@NamedQuery(name = "unit.findAllAncestor", query = "SELECT u FROM Unit u WHERE u.left < :leafLeft and u.right > :leafRight order by u.left")
+	@NamedQuery(name = "unit.findAllChildren", query = "SELECT u FROM Unit u WHERE u.enable = true and u.left > :parentLeft and u.left < :parentRight order by u.left"),
+	@NamedQuery(name = "unit.findAllAncestor", query = "SELECT u FROM Unit u WHERE u.enalbe = true and u.left < :leafLeft and u.right > :leafRight order by u.left")
 })
 public class Unit implements com.kwchina.wfm.domain.common.Entity<Unit> {
 
