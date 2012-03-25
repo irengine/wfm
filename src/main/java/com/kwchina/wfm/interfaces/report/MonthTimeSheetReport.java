da1653a59a38907dd81ab770d3bf08cdbafe4df9
Alex Tang <lalextang@msn.com>
2012-03-25 12:06:09 +0800

    fix new template for month time sheet

--- src/main/java/com/kwchina/wfm/interfaces/report/MonthTimeSheetReport.java
+++ src/main/java/com/kwchina/wfm/interfaces/report/MonthTimeSheetReport.java
@@ -6,29 +6,50 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 
-import com.kwchina.wfm.domain.model.employee.Employee;
 import com.kwchina.wfm.domain.model.employee.TimeSheet;
 
 public class MonthTimeSheetReport {
 	
-	Map<Employee, Map<Date, Set<TimeSheet>>> report = new HashMap<Employee, Map<Date, Set<TimeSheet>>>();
+	private Map<String, Map<Date, Set<TimeSheet>>> data = new HashMap<String, Map<Date, Set<TimeSheet>>>();
+	private List<Date> days;
 
 	public MonthTimeSheetReport() {
 		
 	}
 	
+	public Map<String, Map<Date, Set<TimeSheet>>> getData() {
+		return data;
+	}
+
+	public void setData(Map<String, Map<Date, Set<TimeSheet>>> data) {
+		this.data = data;
+	}
+
+	public List<Date> getDays() {
+		return days;
+	}
+
+	public void setDays(List<Date> days) {
+		this.days = days;
+	}
+
 	public void fill(Set<TimeSheet> list, List<Date> days) {
+		this.setDays(days);
+		
 		for (TimeSheet ts : list) {
-			if (!report.containsKey(ts.getEmployee())) {
-				report.put(ts.getEmployee(), getRows(days));
+			String key = ts.getEmployee().getEmployeeId().toString();
+			
+			if (!getData().containsKey(key)) {
+				getData().put(key, getRows(days));
 			}
-			report.get(ts.getEmployee()).get(ts.getDate()).add(ts);
+			getData().get(key).get(ts.getDate()).add(ts);
 		}
 	}
 	
 	private Map<Date, Set<TimeSheet>> getRows(List<Date> days) {
-		Map<Date, Set<TimeSheet>> rows = new HashMap<Date, Set<TimeSheet>>();
+		Map<Date, Set<TimeSheet>> rows = new TreeMap<Date, Set<TimeSheet>>();
 		for (Date day : days) {
 			rows.put(day, new LinkedHashSet<TimeSheet>());
 		}
